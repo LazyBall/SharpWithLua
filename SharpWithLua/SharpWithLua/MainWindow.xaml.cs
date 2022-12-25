976218e6e765ac40634eb957160f0e561e0376b4
Alexander <informery4@gmail.com>
2022-12-26 00:47:25 +0300

    Добавьте файлы проекта.

---
+++ SharpWithLua/SharpWithLua/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using NLua;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace SharpWithLua
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (Lua luaState = new Lua()) //Создаем новый объект исполнителя Lua
+            {
+                var path = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + Path.DirectorySeparatorChar + "LuaProgram.txt";
+                luaState.DoFile(path);
+                LuaFunction scriptFunc = luaState["gcd"] as LuaFunction;
+                ResultOutput.Content = scriptFunc.Call(int.Parse(txbA.Text), int.Parse(txbB.Text))[0];
+            }
+        }
+    }
+}
